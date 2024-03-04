567c140d99826fdd11e5464f087dddb3408a3fa4
Chee <ck_white@outlook.com>
2024-03-04 18:21:12 -0500

    domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    var paragraph = document.querySelector('p');
+    paragraph.textContent = 'New text';
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var paragraph = document.querySelector('p');
+    paragraph.textContent = 'This is really cool!';
+  });
